3f086a08b4676ff417430317ffa58de36070424e
RegotlyLong <1184508510@qq.com>
2026-07-20 09:40:46 +0800

    123w

--- api/src/main/java/com/github/retrooper/packetevents/protocol/player/User.java
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/player/User.java
@@ -58,6 +58,7 @@ import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
+
 public class User implements IRegistryHolder {
 
     private final Object channel;
