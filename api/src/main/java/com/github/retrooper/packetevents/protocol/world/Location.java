3c3bd8de3ce1b6847d137cbff2e3b5b1430834a7
Regotly <1184508510@qq.com>
2026-08-08 09:05:17 +0800

    location

--- api/src/main/java/com/github/retrooper/packetevents/protocol/world/Location.java
+++ api/src/main/java/com/github/retrooper/packetevents/protocol/world/Location.java
@@ -22,7 +22,10 @@ import com.github.retrooper.packetevents.util.Vector3d;
 import com.github.retrooper.packetevents.util.Vector3f;
 import org.jetbrains.annotations.NotNull;
 
-public class Location {
+
+import java.util.Objects;
+
+public class Location implements Cloneable {
     private Vector3d position;
     private float yaw;
     private float pitch;
@@ -82,7 +85,18 @@ public class Location {
         float z = (float) (xz * Math.cos(Math.toRadians(rotX)));
         return new Vector3f(x, y, z);
     }
-    
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (!(obj instanceof Location)) return false;
+        Location other = (Location) obj;
+        return Objects.equals(this.position, other.position)
+                && this.yaw == other.yaw
+                && this.pitch == other.pitch;
+    }
+
+
     public void setDirection(Vector3f vector) {
         double _2PI = 6.283185307179586D;
         double x = vector.getX();
